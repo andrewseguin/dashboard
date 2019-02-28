d5c6ff0019a499ba0eae6c971e0b1c8fd4da6d2b
Andrew Seguin <andrewjs@google.com>
2019-02-28 05:18:35 +0000

    filter pretriage

--- src/app/content/pre-triage-page/pre-triage-page.ts
+++ src/app/content/pre-triage-page/pre-triage-page.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectionStrategy, Component, ChangeDetectorRef} from '@angular/core';
-import {Issue} from 'app/service/github';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
+import {Issue, Label} from 'app/service/github';
 import {RepoDao} from 'app/service/repo-dao';
 
 @Component({
@@ -15,8 +15,18 @@ export class PreTriagePage {
       if (!repo) {
         return;
       }
+
+      const labels = new Map<number, Label>();
+      repo.labels.forEach(label => labels.set(label.id, label));
+
+      const priorityLabels = ['P0', 'P1', 'P2', 'P3', 'P4', 'P5'];
       this.preTriageIssues = repo.issues.filter(issue => {
-        return true;
+        const isIssue = !issue.pr;
+        const hasPriority = issue.labels.some(label => {
+          return priorityLabels.indexOf(labels.get(+label).name) != -1;
+        });
+
+        return isIssue && !hasPriority;
       });
 
       this.cd.markForCheck();
