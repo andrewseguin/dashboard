9de45369f504c9afb91a626fdfe572b0013b00ce
Andrew Seguin <andrewjs@google.com>
2019-03-02 05:36:11 +0000

    move metadata

--- src/app/content/issues-page/issues-page.ts
+++ src/app/content/issues-page/issues-page.ts
@@ -1,14 +1,11 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import {Issue} from 'app/service/github';
 import {Repo, RepoDao} from 'app/service/repo-dao';
-import {BehaviorSubject, combineLatest, Observable, Subject} from 'rxjs';
-import {map, takeUntil} from 'rxjs/operators';
-
+import {BehaviorSubject, combineLatest, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 import {IssueRecommendations} from '../services/issue-recommendations';
-import {AutocompleteContext, Filter, IFilterMetadata, MatcherContext} from '../utility/search/filter';
-import {DateQuery, InputQuery, NumberQuery, StateQuery} from '../utility/search/query';
-import {dateMatchesEquality, numberMatchesEquality, stateMatchesEquality, stringContainsQuery} from '../utility/search/query-matcher';
-
+import {Filter, MatcherContext} from '../utility/search/filter';
+import {IssuesFilterMetadata} from './issues-filter-metadata';
 
 @Component({
   styleUrls: ['issues-page.scss'],
@@ -84,7 +81,6 @@ export class IssuesPage {
   }
 }
 
-
 export function getIssuesMatchingSearch(
     items: Issue[], repo: Repo, query: string): Issue[] {
   const tokens = query.split(' ');
@@ -97,70 +93,10 @@ export function getIssuesMatchingSearch(
 
 /**
  * Returns a lower-cased string that contains the searchable tokens of Issue.
+ * TODO: Fill in more info, including labels
  */
 export function tokenizeIssue(issue: Issue) {
   const issueStr =
       (issue.title || '') + (issue.body || '') + (issue.reporter || '');
   return issueStr.toLowerCase();
 }
-
-export const IssuesFilterMetadata = new Map<string, IFilterMetadata>([
-
-  /** InputQuery Filters */
-
-  [
-    'title', {
-      displayName: 'Title',
-      queryType: 'input',
-      matcher: (c: MatcherContext, q: InputQuery) => {
-        return stringContainsQuery(c.issue.title, q);
-      },
-      autocomplete: (c: AutocompleteContext) => {
-        return c.repoDao.repo.pipe(map(repo => {
-          return repo.issues.map(issue => issue.title);
-        }));
-      }
-    }
-  ],
-
-  /** NumberQuery Filters */
-
-  [
-    'commentCount', {
-      displayName: 'Comment Count',
-      queryType: 'number',
-      matcher: (c: MatcherContext, q: NumberQuery) => {
-        return numberMatchesEquality(c.issue.comments, q);
-      }
-    }
-  ],
-
-  /** DateQuery Filters */
-
-  [
-    'created', {
-      displayName: 'Date Created',
-      queryType: 'date',
-      matcher: (c: MatcherContext, q: DateQuery) => {
-        return dateMatchesEquality(c.issue.created, q);
-      }
-    }
-  ],
-
-  /** StateQuery */
-
-  [
-    'state', {
-      displayName: 'State',
-      queryType: 'state',
-      queryTypeData: {states: ['open', 'closed']},
-      matcher: (c: MatcherContext, q: StateQuery) => {
-        const values = new Map<string, boolean>([
-          ['open', c.issue.state === 'open'],
-          ['closed', c.issue.state === 'closed'],
-        ]);
-        return stateMatchesEquality(values.get(q.state), q);
-      },
-    }
-  ],
-]);
