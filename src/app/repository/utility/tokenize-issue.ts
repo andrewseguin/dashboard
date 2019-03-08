65246f5e15cab1cd5adb1bcbbb6545b87f2e000b
Andrew Seguin <andrewjs@google.com>
2019-03-08 06:01:45 +0000

    add number to issue

--- src/app/repository/utility/tokenize-issue.ts
+++ src/app/repository/utility/tokenize-issue.ts
@@ -1,11 +1,14 @@
-import {Item} from "app/service/github";
+import {Item} from 'app/service/github';
 
 /**
  * Returns a lower-cased string that contains the searchable tokens of Issue.
  * TODO: Fill in more info, including labels
  */
 export function tokenizeIssue(issue: Item) {
-  const issueStr =
-      (issue.title || '') + (issue.body || '') + (issue.reporter || '');
+  const title = issue.title || '';
+  const body = issue.body || '';
+  const reporter = issue.reporter || '';
+  const issueNumber = issue.number || '';
+  const issueStr = title + body + reporter + issueNumber;
   return issueStr.toLowerCase();
 }
